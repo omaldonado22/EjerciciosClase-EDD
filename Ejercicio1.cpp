c6c8c13da28989a696c878dd17ea6e9a3fda03ee
omaldonado22 <orlynm10@gmail.com>
2023-06-05 09:53:18 -0600

    Si

---
+++ Ejercicio1.cpp
@@ -0,0 +1,30 @@
+//Ordenamiento de burbuja 
+#include <iostream>
+using namespace std;
+
+int main(){
+int arreglo [10];
+for(int x=0; x<10;x++){
+
+    cin>>arreglo[10];
+}
+
+for(int e=0; e<10;e++){
+for(int p=0;p<9; p++){
+if(arreglo[p]>arreglo[p+1]){
+int aux=arreglo[p];
+arreglo[p]=arreglo[p+1];
+arreglo[p+1]=aux;
+
+}
+}
+}
+cout<<"\nImpresion de los elementos del arreglo ordenado\n";
+for(int i=0; i<10;i++){
+cout<<"["<<i<<"]-->"<<arreglo[i]<<"\n";
+}
+
+return 0;
+
+}
+
