93f39c1e8811a2cd56771fde3a813a9d594a7a2f
omaldonado22 <orlynm10@gmail.com>
2023-06-07 10:25:19 -0600

    d

--- EjercicioPilas.cpp
+++ EjercicioPilas.cpp
@@ -1,45 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-
+int main() {
     int arreglo[4][3];
 
-    cout<<"Ingreso de los elementos del arreglo\n";
-  for (int fila = 0; fila < 4; fila++)
-  {
-    for (int col = 0; col < 3; col++)
-    {
-        cin>>arreglo[fila][col];
+    cout << "Ingreso de los elementos del arreglo\n";
+    for (int fila = 0; fila < 4; fila++) {
+        for (int col = 0; col < 3; col++) {
+            cin >> arreglo[fila][col];
+        }
     }
-    
-  }
 
-  cout<<"Impresion de los elementos de los arreglos\n";
-  cout<<"\t\t\tsuma\n";
+    cout << "Impresion de los elementos de los arreglos\n";
+    cout << "\t\t\tsuma\n";
 
-  for (int fila = 0; fila < 4; fila++)
-  {
-   int suma=0;
-   for(int c=0; c<3; c++){
-    cout<<arreglo[fila][c];
-    suma+=arreglo[fila][c];
-   }
-   cout<<suma<<"\n";
+    for (int fila = 0; fila < 4; fila++) {
+        int suma = 0;
+        for (int col = 0; col < 3; col++) {
+            cout << arreglo[fila][col] << "\t";
+            suma += arreglo[fila][col];
+        }
+        cout << suma << "\n";
+    }
 
-  }
-  
-
-  
-    
-return 0;
-
-
-
-
-
-
-
-
-
+    return 0;
 }
